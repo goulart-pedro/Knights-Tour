b6d0de35d72c83dba08761ff6824d7d709e20c66
Pedro Goulart <pedrogoulart555@gmail.com>
2022-09-27 10:46:22 -0300

    get_valid_movements refatorada; gitignore removido

--- knight.c
+++ knight.c
@@ -6,41 +6,45 @@ typedef struct coordinate {
 } coordinate;
 
 // preenche o vetor valid_movements e retorna a quantidade de itens adicionados
-int get_valid_movements(int n, coordinate tour[], coordinate valid_movements[], coordinate c) {
+int tour_find(coordinate tour[], int tour_size, coordinate c)
+{
+  // determina se as coordenadas ja se encontram no tour
+  int is_mov_in_tour = 0;
+  // percorre o tour
+  for(int i=0; i<tour_size; i++) {
+    is_mov_in_tour =
+     tour[i].x == c.x
+     && tour[i].y == c.y;
+
+    if(is_mov_in_tour)
+      break;
+  }
+
+  return is_mov_in_tour;
+}
+
+// determina se as coordenadas nao levam a uma posicao fora do tabuleiro
+int are_coord_in_bounds(coordinate test_coord)
+{
+  return  (test_coord.y >= 0 && test_coord.y < 8)
+   && (test_coord.x >= 0 && test_coord.x < 8);
+}
+
+int get_next_movements(int tour_size, coordinate tour[], coordinate valid_movements[], coordinate curr_pos) {
   int moves_y[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
   int moves_x[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
-  int mov_insertion_idx = 0;
   int movements_length = 0;
-  // percorre as opcoes de variacao de posicao
   for(int i=0; i<8; i++) {
-    // determina se as coordenadas ja se encontram no tour
-    int is_mov_in_tour = 0;
-    // percorre o tour
-    for(int j=0; j<n; j++) {
-      is_mov_in_tour = 
-        tour[j].x == c.x + moves_x[i] &&
-        tour[j].y == c.y + moves_y[i];
+    coordinate test_coord = {curr_pos.x + moves_x[i], curr_pos.y + moves_y[i]};
+    int is_mov_in_tour = tour_find(tour, tour_size, test_coord);
+    int coord_in_bounds = are_coord_in_bounds(test_coord);
 
-      if(is_mov_in_tour)
-        break;
+    if(!is_mov_in_tour && coord_in_bounds) {
+      valid_movements[movements_length++] = test_coord;
     }
 
-    // determina se as coordenadas nao levam a uma posicao fora do tabuleiro
-    // caso verdadeiro, adiciona as coordenadas aos movimentos validos
-    if(
-        !is_mov_in_tour &&
-        (c.y + moves_y[i] >= 0 && c.y + moves_y[i] < 8) && 
-        (c.x + moves_x[i] >= 0 && c.x + moves_x[i] < 8)
-      ) {
-      movements_length++;
-      coordinate new_move;
-      new_move.x = c.x + moves_x[i];
-      new_move.y = c.y + moves_y[i];
-      valid_movements[mov_insertion_idx++] = new_move;
-    }
-  
-  }  
+  }
   return movements_length;
 }
 
@@ -63,18 +67,8 @@ int knight(coordinate tour[], coordinate c, int n)
   return 0;
 }
 
-
-int main(int argc, char** argv)
+void board_print(int board[8][8], coordinate tour[])
 {
-  coordinate tour[64];
-  coordinate c = {atoi(argv[1]), atoi(argv[2])};
-  if(!knight(tour, c, 0)) {
-    printf("Possibilidades exauridas; Nenhum passeio encontrado.\n");
-    return 0;
-  }
-
-  // tabuleiro para mostrar o tour
-  int board[8][8];
   for(int i=0; i<64; i++){
     board[tour[i].x][tour[i].y] = i+1;
   }
@@ -94,6 +88,19 @@ int main(int argc, char** argv)
     else  
       printf("%d\n", board[i][7]);
   }
-  
+}
+
+int main(int argc, char** argv)
+{
+  coordinate tour[64];
+  coordinate c = {atoi(argv[1]), atoi(argv[2])};
+  if(!knight(tour, c, 0)) {
+    printf("Possibilidades exauridas; Nenhum passeio encontrado.\n");
+    return 0;
+  }
+
+  // tabuleiro para mostrar o tour
+  int board[8][8];
+  board_print(board, tour) ;
 }
 
