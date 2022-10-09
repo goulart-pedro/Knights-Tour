96a00b0a43525c7e43983dd852b73863685d3416
Pedro Goulart <pedrogoulart555@gmail.com>
2022-10-09 11:32:54 -0300

    implementacao otimizada

--- knight.c
+++ knight.c
@@ -5,22 +5,11 @@ typedef struct coordinate {
   int x, y;
 } coordinate;
 
-// acha uma coordenado no tour
-int tour_find(coordinate tour[], int tour_size, coordinate c)
+// Verifica se uma coordenada esta presente no tour 
+// Pre-condicao:  coordenada c eh valida
+int is_in_tour(int tour[8][8], coordinate c)
 {
-  // determina se as coordenadas ja se encontram no tour
-  int is_mov_in_tour = 0;
-  // percorre o tour
-  for(int i=0; i<tour_size; i++) {
-    is_mov_in_tour =
-     tour[i].x == c.x
-     && tour[i].y == c.y;
-
-    if(is_mov_in_tour)
-      break;
-  }
-
-  return is_mov_in_tour;
+  return tour[c.y][c.x] != -1;
 }
 
 // determina se as coordenadas nao levam a uma posicao fora do tabuleiro
@@ -31,18 +20,18 @@ int are_coord_in_bounds(coordinate test_coord)
 }
 
 // preenche o vetor mov_buffer retorna a quantidade de itens adicionados
-int get_next_movements(int tour_size, coordinate tour[], coordinate mov_buffer[], coordinate curr_pos) {
+int get_next_movements(int tour_size, int tour[8][8], coordinate mov_buffer[], coordinate curr_pos) {
   coordinate moves[] = {
     {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}, {-2,1}, {-1,2} 
   };
 
   int movements_length = 0;
-  for(int i=0; i<8; i++) {
+  for (int i=0; i<8; i++) {
     coordinate test_coord = {curr_pos.x + moves[i].x, curr_pos.y + moves[i].y};
-    int is_mov_in_tour = tour_find(tour, tour_size, test_coord);
+    int is_mov_in_tour = is_in_tour(tour, test_coord);
     int coord_in_bounds = are_coord_in_bounds(test_coord);
 
-    if(!is_mov_in_tour && coord_in_bounds) {
+    if(coord_in_bounds && !is_mov_in_tour) {
       mov_buffer[movements_length++] = test_coord;
     }
 
@@ -50,26 +39,23 @@ int get_next_movements(int tour_size, coordinate tour[], coordinate mov_buffer[]
   return movements_length;
 }
 
-int knight(coordinate tour[], coordinate c, int n, long long int* backtracks, int* fail)
+int knight(int tour[8][8], coordinate c, int n, long long int* backtracks, int* fail)
 {
-  tour[n] = c;
+  tour[c.y][c.x] = n;
   if(n == 63)
     return 1;
 
   // definicao de proximos movimentos validos 
   coordinate valid_moves[8];
   int movements_length = get_next_movements(n, tour, valid_moves, c);
-  if(movements_length == 0)
-  {
-    *fail = 1;
-    return 0;
-  }
+    
 
-  for(int i=0; i<movements_length; i++) {
+  for (int i=0; i<movements_length; i++) {
     if(*fail)
     {
       *fail = 0;
-      // *(backtracks)++;
+      tour[c.y][c.x] = -1;
+      *(backtracks) += 1;
       return 0;
     }
 
@@ -80,18 +66,20 @@ int knight(coordinate tour[], coordinate c, int n, long long int* backtracks, in
     *(backtracks) += 1;
     n--;
   }
+
+  tour[c.y][c.x] = -1;
+  *fail = 1;
   return 0;
 }
 
-void board_print(int board[8][8], coordinate tour[])
+void board_print(int board[8][8])
 {
-  for(int i=0; i<64; i++){
-    board[tour[i].x][tour[i].y] = i+1;
-  }
-
   // printando o tabuleiro
-  for(int i=0; i<8; i++) {
-    for(int j=0; j<7; j++) {
+  for (int i=0; i<8; i++) {
+    for (int j=0; j<7; j++) {
+      // incrementando para converter de indice 0 para 1
+      // board[i][j]++;
+
       // verifica se o numero tem apenas um digito
       // se sim, printa o numero com um zero a frente
       if(board[i][j] /10 < 1)
@@ -110,16 +98,21 @@ int main(int argc, char** argv)
 {
   int fail = 0;
   long long int backtracks = 0;
-  coordinate tour[64];
-  coordinate c = {atoi(argv[1]), atoi(argv[2])};
-  if(!knight(tour, c, 0, &backtracks, &fail)) {
+  // -1 para conversao de indice 1 para 0
+  // coordinate c = {atoi(argv[1] -1), atoi(argv[2] -1)};
+  coordinate c = {atoi(argv[2]), atoi(argv[1])};
+  // inicializacao do tabuleiro
+  int board[8][8];
+  for (int i=0; i<8; i++)
+    for (int j=0; j<8; j++)
+      board[i][j] = -1; // valor '-1' representa uma posicao disponivel
+
+  if(!knight(board, c, 0, &backtracks, &fail)) {
     printf("Possibilidades exauridas; Nenhum passeio encontrado.\n");
     return 0;
   }
 
   // tabuleiro para mostrar o tour
-  int board[8][8];
-  board_print(board, tour) ;
-  printf("Retrocidido: %llu vezes\n", backtracks);
+  board_print(board) ;
+  printf("Retrocedido: %llu vezes\n", backtracks);
 }
-
